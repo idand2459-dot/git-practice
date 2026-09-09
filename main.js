a8aad152a23f28b0113e15fb9f3b19c4a8da1281
Idan <idan@example.com>
2026-09-09 23:43:18 +0300

    add maxdiameter

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-function onBallClick() {
-    const elBall = document.querySelector('.ball');
+function onBallClick(elBall, maxDiameter) {
+    console.log('elBall received:', elBall); // נוודא שהוא מקבל את הכדור
     
     const currentSize = parseInt(elBall.style.width) || 100;
-    let randomNum=getRandomIntInt(20,60);
-    let randomColor=getRandomColor();
+    let randomNum = getRandomIntInt(20, 60);
+    let randomColor = getRandomColor();
     let newSize = currentSize + randomNum;
     
-    if (newSize > 400) {
+    if (newSize > maxDiameter) {
         newSize = 100;
     }
     
     elBall.style.width = newSize + 'px';
     elBall.style.height = newSize + 'px';
-    elBall.style.backgroundColor=randomColor;
-    
+    elBall.style.backgroundColor = randomColor;
     elBall.innerText = newSize;
 }
