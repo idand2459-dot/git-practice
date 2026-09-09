3887012f24c72ca09817347b86ec6ff4daafa48b
Idan <idan@example.com>
2026-09-09 22:23:14 +0300

    Add style and click to ball

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 function onBallClick(){
+const elBall=document.querySelector('.ball')
 
-    console.log('Ball clicked!')
+const currentSize=parseInt(elBall.style.width)||100;
+const newSize=currentSize + 50;
+
+elBall.style.width=currentSize + 'px'
+elBall.style.height=currentSize + 'px'
+   
+elBall.innerText=newSize;
 }
