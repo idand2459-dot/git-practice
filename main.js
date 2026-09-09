733227759c6a6ae0d18ada877c85fb688447b6e1
Idan <idan@example.com>
2026-09-09 23:12:33 +0300

    Add getRandomInt and getRandomColor,update onBallClick

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ function onBallClick() {
     const elBall = document.querySelector('.ball');
     
     const currentSize = parseInt(elBall.style.width) || 100;
-    let newSize = currentSize + 50;
+    let randomNum=getRandomIntInt(20,60);
+    let randomColor=getRandomColor();
+    let newSize = currentSize + randomNum;
     
     if (newSize > 400) {
         newSize = 100;
@@ -10,6 +12,7 @@ function onBallClick() {
     
     elBall.style.width = newSize + 'px';
     elBall.style.height = newSize + 'px';
+    elBall.style.backgroundColor=randomColor;
     
     elBall.innerText = newSize;
 }
---
+++ util.js
@@ -0,0 +1,14 @@
+function getRandomIntInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
